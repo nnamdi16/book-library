76e7c0cda883e090a341cc62e488be994aee0d75
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-10 22:48:59 +0100

    Add the access to issuance.js

--- admin.js
+++ admin.js
@@ -2,6 +2,8 @@ const Person = require('./person');
 const Library = require('./library');
 const RequestList = require('./requestList');
 const userState = require('./user');
+const student = require('./student');
+const issuance = require('./issuanceList');
 
 //Create the Library's Admin constructor function
 function Admin(name) {
@@ -24,8 +26,8 @@ Admin.prototype.addBook = function(bookName,author,quantity) {
 const admin = new Admin('Nnamdi');
 admin.addBook('Tears of Shadows', 'Peter Obi',5);
 admin.addBook('Sherlock Holmes','John Paul', 6)
-console.log(admin);
-console.log(Library);
+// console.log(admin);
+// console.log(Library);
 console.log(RequestList);
-console.log(Library['Tears of Shadows'][2]);
+// console.log(Library['Tears of Shadows'][2]);
 module.exports = Admin;
