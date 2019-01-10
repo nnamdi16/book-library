e9b16363cb746877fba8f39ee28e2778054448f6
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-10 09:11:53 +0100

    Add test for the Person Constructor function and update the description of each test

--- index.test.js
+++ index.test.js
@@ -1,7 +1,8 @@
 const Lib = require('./index');
+const Person = require('./person');
 const books = Lib.two;
 const Library = Lib.one;
-describe('Check that an object is an instance of a class', () => {
+describe('Check that an object is an instance of the Library Class', () => {
   const book1 = new Library('JavaScript', 2, 23, 'Nwabuokei Nnamdi');
  
   test('should return the instance of a class', () => {
@@ -12,3 +13,11 @@ describe('Check that an object is an instance of a class', () => {
   });
 });
 
+
+describe('Check that an object is an instance of the Person class', () => {
+  const person = new Person('Nnamdi');
+
+  test('should return the instance of a class', () => {
+    expect(person).toBeInstanceOf(Person);
+  });
+});
