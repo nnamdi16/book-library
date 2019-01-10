d9b7952b74729681d49c8752fdf39ed23c0fe697
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-10 15:37:15 +0100

    Create access to the user.js and student.js file

--- admin.js
+++ admin.js
@@ -1,14 +1,31 @@
 const Person = require('./person');
+const Library = require('./library');
+const RequestList = require('./requestList');
+const userState = require('./user');
 
 //Create the Library's Admin constructor function
 function Admin(name) {
 	Person.apply(this,[name]);
+	// this.bookName = bookName;
+	// this.author = author;
+
 }
 
 Admin.prototype = Object.create(Person.prototype);
 Admin.prototype.constructor = Admin;
 
-const admin = new Admin('Nnamdi');
-console.log(admin);
+//Create an Add book Method
+Admin.prototype.addBook = function(bookName,author,quantity) {
+	Library[bookName] = [bookName,author,quantity];
+	// Library["quantity"] = quantity;
+}
 
+
+const admin = new Admin('Nnamdi');
+admin.addBook('Tears of Shadows', 'Peter Obi',5);
+admin.addBook('Sherlock Holmes','John Paul', 6)
+console.log(admin);
+console.log(Library);
+console.log(RequestList);
+console.log(Library['Tears of Shadows'][2]);
 module.exports = Admin;
