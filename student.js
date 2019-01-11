d3449c127016a054b7b5a5013d6386d06ac63fb4
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-11 04:28:53 +0100

    Modify the borrow method

--- student.js
+++ student.js
@@ -1,7 +1,8 @@
 const User = require('./user');
 const BorrowBook= require('./requestList');
 const issuance = require('./issuanceList');
-const firstStudent = issuance.Student;
+const firstStudent = issuance.Senior;
+const secondStudent = issuance.Junior;
 
 //Create the Student constructor function
 function Student(name,status,studentLevel) {
@@ -13,18 +14,21 @@ function Student(name,status,studentLevel) {
 Student.prototype =Object.create(User.prototype);
 Student.prototype.constructor = Student;
 
-const student = new Student('Nnmadi', 'Student','Senior');
-const student1 = new Student('Balogun', 'Student', 'Junior');
-const student2 = new Student('Moses','Student', 'Senior');
-// console.log(student);
+
 
 User.prototype.borrowBook =  function (bookName,author='unknown') {
 	BorrowBook[this.name] = [this.status,this.studentLevel,bookName,author];
-	firstStudent[this.name] = [this.status,this.studentLevel,bookName,author];
+	
 }
 
-student.borrowBook('Sherlock Holmes','Peter Obi');
-student1.borrowBook('Tales of Thames', 'Chinua Achebe');
-student2.borrowBook('Alexander in the Wonderland', 'Flavio Copes');
+
+// const student = new Student('Nnmadi', 'Student','Senior');
+// const student1 = new Student('Balogun', 'Student', 'Junior');
+// const student2 = new Student('Moses','Student', 'Senior');
+//  console.log(student);
+
+// student.borrowBook('Sherlock Holmes','Peter Obi');
+// student1.borrowBook('Tales of Thames', 'Chinua Achebe');
+// student2.borrowBook('Alexander in the Wonderland', 'Flavio Copes');
 // console.log(BorrowBook);
 module.exports = Student;
