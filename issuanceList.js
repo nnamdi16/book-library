e76148235f1b125ab83cc14947c415053f1682d9
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-11 04:25:30 +0100

    Destructure the the student variable to accommodate student levels

--- issuanceList.js
+++ issuanceList.js
@@ -1,1 +1,14 @@
-const issuanceList = {};
+let senior = [];
+let junior = [];
+let teacher = [];
+let student = [senior,junior];
+[senior,junior] = student;
+// let studentLevel = [];
+// const issuanceList = [teacher,student];
+
+exports.Teacher = teacher;
+exports.Senior = student[0];
+exports.Junior = student[1];// exports.Student = student;
+
+// exports.Student.Senior = studentLevel;
+// exports.third = issuanceList
