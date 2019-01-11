859ee829048a911b441871b7b9a8fc433be191bc
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-11 15:04:54 +0100

    Update and  create more instance of teacher

--- teacher.js
+++ teacher.js
@@ -13,13 +13,15 @@ Teacher.prototype = Object.create(User.prototype);
 Teacher.prototype.constructor = Teacher;
 
 const teacher = new Teacher('Nnamdi','Teacher');
+const teacher1 = new Teacher('Sopuluchukwu','Teacher');
 // console.log(teacher);
 
 User.prototype.borrowBook =  function (bookName,author='unknown') {
-	BorrowBook[`${bookName} ${author}`] = [this.name];
-	firstTeacher[`${bookName} ${author}`] = [this.name];
+	BorrowBook[`${bookName} by ${author}`] = [this.name];
+	firstTeacher[`${bookName} by ${author}`] = [this.name];
 }
 
 teacher.borrowBook('Harry Porter','James Roslyn') ;
+teacher1.borrowBook('Shades of Purple','Peter Obi');
 
 module.exports = Teacher;
