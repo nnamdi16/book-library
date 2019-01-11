afe0bf3254857788a0565cec3658d577103d8f26
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-11 01:18:09 +0100

    Modify the teacher and firstStudent variable assignment to reflect the issuance.js file for access to the exported variable

--- admin.js
+++ admin.js
@@ -2,8 +2,13 @@ const Person = require('./person');
 const Library = require('./library');
 const RequestList = require('./requestList');
 const userState = require('./user');
+const teacherState = require('./teacher');
 const student = require('./student');
 const issuance = require('./issuanceList');
+const teacher = issuance.Teacher;
+const firststudent = issuance.Student;
+// const listIssuance = issuance.third;
+
 
 //Create the Library's Admin constructor function
 function Admin(name) {
@@ -26,8 +31,16 @@ Admin.prototype.addBook = function(bookName,author,quantity) {
 const admin = new Admin('Nnamdi');
 admin.addBook('Tears of Shadows', 'Peter Obi',5);
 admin.addBook('Sherlock Holmes','John Paul', 6)
+
+
 // console.log(admin);
 // console.log(Library);
-console.log(RequestList);
+// console.log(`${RequestList}`);
+var result = Object.keys(RequestList).map(function(key) {
+	return [(key), RequestList[key]];
+  });
+  
+  console.log(result);
+  console.log(issuance);
 // console.log(Library['Tears of Shadows'][2]);
 module.exports = Admin;
