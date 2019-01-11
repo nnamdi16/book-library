b59f844fd7c010d5f230b7d6fc29e7ad5ba44805
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-11 01:19:52 +0100

    Override the borrowBook method and modify the first Teacher variable

--- teacher.js
+++ teacher.js
@@ -1,4 +1,7 @@
 const User = require('./user');
+const BorrowBook= require('./requestList');
+const issuance = require('./issuanceList');
+const firstTeacher = issuance.Teacher;
 
 //Create constructor function for the Teacher
 function Teacher(name,status) {
@@ -10,5 +13,13 @@ Teacher.prototype = Object.create(User.prototype);
 Teacher.prototype.constructor = Teacher;
 
 const teacher = new Teacher('Nnamdi','Teacher');
-console.log(teacher);
+// console.log(teacher);
+
+User.prototype.borrowBook =  function (bookName,author='unknown') {
+	BorrowBook[this.name] = [this.status,bookName,author];
+	firstTeacher[this.name] = [this.status,bookName,author];
+}
+
+teacher.borrowBook('Harry Porter','James Roslyn') ;
+
 module.exports = Teacher;
