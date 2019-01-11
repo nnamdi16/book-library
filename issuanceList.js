8677684f2a5dcc6059c0d4a148c8f73f94542b7e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-11 14:59:09 +0100

    Modified the exported object variables

--- issuanceList.js
+++ issuanceList.js
@@ -1,14 +1,9 @@
-let senior = [];
-let junior = [];
-let teacher = [];
-let student = [senior,junior];
-[senior,junior] = student;
-// let studentLevel = [];
-// const issuanceList = [teacher,student];
+let senior = {};
+let junior = {};
+let teacher = {};
 
 exports.Teacher = teacher;
-exports.Senior = student[0];
-exports.Junior = student[1];// exports.Student = student;
 
-// exports.Student.Senior = studentLevel;
-// exports.third = issuanceList
+exports.Senior = senior;
+exports.Junior = junior;
+
