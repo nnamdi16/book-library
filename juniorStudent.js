98fb526032a27b0f371f677f7edf917d6449ee1b
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-13 15:09:22 +0100

    Comment sections of the code for clarity

--- juniorStudent.js
+++ juniorStudent.js
@@ -19,12 +19,13 @@ User.prototype.borrowBook =  function (bookName,author='unknown') {
 		return `${bookName} by ${author}`;
 }
 	
-
+//Instance of junior student personal details.
 const student = new Junior('Saboginda', 'Student','Junior');
 const student1 = new Junior('Galadime', 'Student', 'Junior');
 const student2 = new Junior('Boluwa','Student', 'Junior');
 
 
+//Instance of junior student borrowing book.
 student.borrowBook('Sherlock Holmes','Peter Obi');
 student1.borrowBook('Tales of Thames', 'Chinua Achebe');
 student2.borrowBook('Alexander in the Wonderland', 'Flavio Copes');
