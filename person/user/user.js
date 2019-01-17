77c130866686566f64631f83f889926b885cff43
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-17 03:03:39 +0100

    Create the user.js constructor function and the borrowBook prototype method

---
+++ person/user/user.js
@@ -0,0 +1,21 @@
+//Import Person module to user.js
+const Person = require('../person');
+const requestList = require('../../data/requestList');
+
+//Create the user constructor function
+function User (name,status) {
+	Person.apply (this,[name]);
+	this.status = status;
+}
+
+User.prototype = Object.create(Person.prototype);
+User.prototype.constructor = User;
+
+User.prototype.borrowBook = function (bookName,author) {
+	return requestList.push({
+		[`${bookName} by ${author}`]: this.name,
+	})
+};
+
+
+module.exports = User;
