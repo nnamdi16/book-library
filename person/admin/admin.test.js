b64d2d90d756155791f9b24c2ec6c4ef8bc361a8
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-17 11:12:14 +0100

    Create test case for issue book prototype method

--- person/admin/admin.test.js
+++ person/admin/admin.test.js
@@ -1,8 +1,10 @@
 //Import files from admin.js
 const Admin = require('./admin');
 const library = require('../../data/library');
+const issuedBook = require('../../data/issueList');
+const requestList = require('../../data/requestList');
 
-//Test to check for instance creation of the admin constructor function.
+//Test case to check for instance creation of the admin constructor function.
 describe('Check if an instance of the Admin constructor is created', () => {
 	it('Should create an instance of the Admin constructor', () => {
 		const admin = new Admin('Galvin Belson');
@@ -17,11 +19,23 @@ describe('Check if an instance of the Admin constructor is created', () => {
 
 });
 
+//Test case to test the  addBook prototype method.
 describe('Create book when the name of the book is added',() =>{
 	it('Should create an array of object of new books whe instantiated', () => {
 		const admin = new Admin('Galvin Belson');
 		admin.addBook('Spiral Shift Island','Anslem Crenshaw', 9);
 		const result = [{'Spiral Shift Island by Anslem Crenshaw':9}];
 		expect(result).toEqual(library);
-	})
-})
+	});
+});
+
+//Test case to test the issue book prototype method.
+describe('Issues book  from the request list', () => {
+	it('Should create an array of object stating the user issued book to and the name of the book', () =>{
+		const admin = new Admin('Galvin Belson');
+		admin.issueBook ();
+		const result = [{'Spiral Shift Island by Anslem Crenshaw':'Nnamdi' ,
+						'Books remaining': 8}];
+		expect(result).toEqual(issuedBook)
+	} );
+});
