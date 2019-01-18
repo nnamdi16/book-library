47de73dd1fc75fc4fec3b9caf9d27f8b7e9e2b28
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-18 11:47:18 +0100

    Modify the expected output of the object for the borrow book prototype method

--- person/user/student/senior_student/seniorStudent.test.js
+++ person/user/student/senior_student/seniorStudent.test.js
@@ -5,15 +5,16 @@ const requestList = require('../../../../data/requestList');
 //Test cases to test the  Senior student constructor function.
 describe('Check if an instance of the senior student constructor is created', () => {
 	it('Should create an instance of the senior student constructor', () => {
-		const seniorStudent = new SeniorStudent('John Bash','Student','Senior');
+		const seniorStudent = new SeniorStudent('John Bash','Student','Senior',2);
 		const result = {'name':'John Bash',
 						'status':'Student',
-						'studentLevel':'Senior'}
+						'studentLevel':'Senior',
+						'priority':2}
 		expect(seniorStudent).toEqual(result)
 	});
 
 	it('Check if the Object created is an instance of the senior student Constructor', () => {
-		const seniorStudent= new SeniorStudent('John Bash','Student','Senior');
+		const seniorStudent= new SeniorStudent('John Bash','Student','Senior',2);
 		expect(seniorStudent).toBeInstanceOf(SeniorStudent);
 	});
 
@@ -21,10 +22,13 @@ describe('Check if an instance of the senior student constructor is created', ()
 
 describe('Check if an instance of the borrowBook method is created', () => {
 	it('Should create an instance of the borrow book prototype method', () => {
-		const student= new SeniorStudent('John Bash','Student','Student');
+		const student= new SeniorStudent('John Bash','Student','Student',2);
 		student.borrowBook('Alexander in the wonderland', 'Sophie Dash');
-		const result = [{'Alexander in the wonderland by Sophie Dash' : 'John Bash',
-						'Priority':2}];
+		const result = [{'BookName':'Alexander in the wonderland',
+		'BookLender':'John Bash',
+		'Author':'Sophie Dash',
+		'Priority':2
+		}];
 		expect(requestList).toEqual(result);
 	});
 
