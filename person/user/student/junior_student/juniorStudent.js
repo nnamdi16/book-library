0cdf1fa97c61563b3f580e32ce19d55ae84cd9b4
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-18 11:43:06 +0100

    Modify the expected output of the object for the borrow book prototype method

--- person/user/student/junior_student/juniorStudent.js
+++ person/user/student/junior_student/juniorStudent.js
@@ -5,8 +5,9 @@ const requestList = require('../../../../data/requestList');
 
 
 //Create the Junior Student constructor function
-function JuniorStudent(name,status,studentLevel) {
-	Student.apply(this,[name,status,studentLevel]);		
+function JuniorStudent(name,status,studentLevel,priority) {
+	Student.apply(this,[name,status,studentLevel]);
+	this.priority = priority;		
 }
 
 JuniorStudent.prototype =Object.create(Student.prototype);
@@ -16,9 +17,13 @@ JuniorStudent.prototype.constructor = JuniorStudent;
  //Override the borrow prototype method from the User constructor function.
 User.prototype.borrowBook =  function (bookName,author) {
 		requestList.push({
-			[`${bookName} by ${author}`]: this.name,
-			'Priority' : 3
+			'BookName' : bookName,
+			'BookLender': this.name,
+			'Author':author,
+			'Priority' :  this.priority
+			
 		});
+		
 	}
 
 module.exports = JuniorStudent;
