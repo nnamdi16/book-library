dedf94ded330b5cb8f866b26d15814ebbe9fbdb6
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-18 11:37:37 +0100

    Create the issueBook prototype method using binary search algorithm

--- person/admin/admin.js
+++ person/admin/admin.js
@@ -1,6 +1,8 @@
 //Import the Person module 
 const Person = require('../person');
 const library = require('../../data/library');
+const requestList = require('../../data/requestList');
+const issueList = require('../../data/issueList');
 
 //Create admin constructor function
 function Admin(name) {
@@ -14,8 +16,52 @@ Admin.prototype.constructor = Admin;
 //Create the addBook Prototype method.
 Admin.prototype.addBook = function (bookName,author,quantity) {
 	library.push({
-		[`${bookName} by ${author}`] : quantity
+		// [`${bookName} by ${author}`] : quantity
+		'BookName' : bookName,
+		'Author': author, 
+		'Quantity':quantity
+
 	})
+}
+
+
+  function binarySearch(arr, target) {
+	// console.log(target)
+	  let left = 0;
+	  let right = arr.length - 1;
+	  while (left <= right) {
+		  const mid = left + Math.floor((right - left) / 2);
+		  if (arr[mid].BookName === target && arr[mid].Quantity>0) {
+				arr[mid].Quantity = arr[mid].Quantity-0.5;
+				return arr[mid];
+				
+		  }
+		  if (arr[mid].BookName < target) {
+			  left = mid + 1;
+		  } else {
+			  right = mid - 1;
+		  }
+	  }
+	  return 0;
+  }
+
+Admin.prototype.issueBook = function() {
+	requestList.sort(function(a,b){
+		return String(a['Priority']).localeCompare(String(b['Priority']));	
+	});
+
+	for (let i = 0; i < requestList.length; i++) {
+		if(binarySearch(library,requestList[i].BookName)){
+			console.log(`${requestList[i].BookName} issued to  ${requestList[i].BookLender}`);
+			issueList.push(binarySearch(library, requestList[i].BookName));
+		} else{
+			console.log(`${requestList[i].BookLender} : ${requestList[i].BookName} not Found or Taken `)
+		}
+		
+	  }
+
+	// let result = requestList.filter(o => library.find(o2 => o['Book Name'] === o2['Book Name']));
+	// console.log('Final Result',result);
 }
 
 module.exports = Admin;
