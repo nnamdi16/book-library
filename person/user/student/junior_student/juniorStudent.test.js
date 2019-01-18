8076dadfa25d6298c18e94c35388a221283b7839
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-18 11:44:45 +0100

    Modify what should be expected for the borrow method request method

--- person/user/student/junior_student/juniorStudent.test.js
+++ person/user/student/junior_student/juniorStudent.test.js
@@ -5,15 +5,16 @@ const requestList = require('../../../../data/requestList');
 //Test cases to test the  Junior student constructor function.
 describe('Check if an instance of the junior student constructor is created', () => {
 	it('Should create an instance of the senior student constructor', () => {
-		const juniorStudent = new JuniorStudent('Anthony Klemann','Student','Junior');
+		const juniorStudent = new JuniorStudent('Anthony Klemann','Student','Junior',3);
 		const result = {'name':'Anthony Klemann',
-						'status':'Student',
-						'studentLevel':'Junior'}
+		'status':'Student',
+		'studentLevel':'Junior',
+		'priority':3}
 		expect(juniorStudent).toEqual(result)
 	});
 
 	it('Check if the Object created is an instance of the junior student Constructor', () => {
-		const juniorStudent = new JuniorStudent('Anthony Klemann','Student','Junior');
+		const juniorStudent = new JuniorStudent('Anthony Klemann','Student','Junior',3);
 		expect(juniorStudent).toBeInstanceOf(JuniorStudent);
 	});
 
@@ -21,14 +22,17 @@ describe('Check if an instance of the junior student constructor is created', ()
 
 describe('Check if an instance of the borrowBook method is created', () => {
 	it('Should create an instance of the borrow book prototype method', () => {
-		const juniorStudent= new JuniorStudent('John Bash','Student','Junior');
+		const juniorStudent= new JuniorStudent('John Bash','Student','Junior',3);
 		juniorStudent.borrowBook('Alexander in the wonderland', 'Sophie Dash');
-		const result = [{'Alexander in the wonderland by Sophie Dash' : 'John Bash',
-						'Priority':3}];
+		const result = [{'BookName':'Alexander in the wonderland',
+		'BookLender':'John Bash',
+		'Author':'Sophie Dash',
+		'Priority':3
+		}];
 		expect(requestList).toEqual(result);
 	});
 
-	it('Check if the Object created is an instance of SeniorStudent Constructor', () => {
+	it('Check if the Object created is an instance of JuniorStudent Constructor', () => {
 		const juniorStudent= new JuniorStudent('John Bash','Student','Junior');
 		expect(juniorStudent).toBeInstanceOf(JuniorStudent);
 	});
