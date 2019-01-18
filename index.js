aa744cd6b85a01ff63dad8c58d40d8ad9e10fa0f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-18 16:12:19 +0100

    Update data

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Admin = require('./person/admin/admin');
 const library = require('./data/library');
 const SeniorStudent = require('./person/user/student/senior_student/seniorStudent');
 const JuniorStudent = require('./person/user/student/junior_student/juniorStudent');
+const returnList = require('./data/returnList');
 
 const user1 = new JuniorStudent('Nnamdi','Student','junior',1);
 const user4 = new Teacher ('Shakira','Teacher',3);
@@ -33,5 +34,12 @@ admin.addBook('Simple Crazy', 'Antolva Cripal',8);
 console.log('Request',requestList);
 admin.issueBook();
 // console.log('Request',requestList);
-console.log('Lib',library);
+// console.log('Lib',library);
 // console.log(issueList);
+user1.returnBook('Simple Crazy', 'Antolva Cripal');
+user3.returnBook('Simple Crazy', 'Antolva Cripal');
+
+console.log('Library return', returnList);
+admin.replaceBook();
+
+console.log('library',library);
