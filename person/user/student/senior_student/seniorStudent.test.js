2e9e5d665be1d2c45de03e91ef7d8a58913f538b
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-18 17:14:34 +0100

    Update the test case for the senior_student.test cases

--- person/user/student/senior_student/seniorStudent.test.js
+++ person/user/student/senior_student/seniorStudent.test.js
@@ -1,6 +1,7 @@
 //Import modules.
 const SeniorStudent = require('./seniorStudent');
 const requestList = require('../../../../data/requestList');
+const returnList = require('../../../../data/returnList')
 
 //Test cases to test the  Senior student constructor function.
 describe('Check if an instance of the senior student constructor is created', () => {
@@ -20,6 +21,7 @@ describe('Check if an instance of the senior student constructor is created', ()
 
 });
 
+//Test cases to check  method of borrowed books
 describe('Check if an instance of the borrowBook method is created', () => {
 	it('Should create an instance of the borrow book prototype method', () => {
 		const student= new SeniorStudent('John Bash','Student','Student',2);
@@ -35,6 +37,25 @@ describe('Check if an instance of the borrowBook method is created', () => {
 	it('Check if the Object created is an instance of SeniorStudent Constructor', () => {
 		const student= new SeniorStudent('John Bash','Student','Senior');
 		expect(student).toBeInstanceOf(SeniorStudent);
+	});
+
+	//Test cases to check the returnList method.
+	describe('Check if an instance of the returnList method is created', () => {
+		it('Should create an instance of the return book list prototype method', () => {
+			const seniorStudent = new SeniorStudent('Harvey Spectre','Teacher',2);
+			seniorStudent.returnBook('Sherlock Holmes', 'Peter Obi');
+			const result = [{'BookName':'Sherlock Holmes',
+			'BookLender':'Harvey Spectre',
+			'Author':'Peter Obi',
+			}];
+			expect(returnList).toEqual(result);
+		});
+	
+		it('Check if the Object created is an instance of the teacher Constructor', () => {
+			const seniorStudent = new SeniorStudent('Harvey Spectre','Teacher',1);
+			expect(seniorStudent).toBeInstanceOf(SeniorStudent);
+		});
+	
 	});
 
 });
