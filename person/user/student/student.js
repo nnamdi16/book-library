c4dffc7dc855d4fea38189d67064c123ea07be75
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-18 17:19:28 +0100

    Create a returnList method

--- person/user/student/student.js
+++ person/user/student/student.js
@@ -3,34 +3,34 @@ const requestList = require('../../../data/requestList');
 const returnList = require('../../../data/returnList');
 
 //Create the Student constructor function
-function Student(name,status,studentLevel) {
-	User.apply(this,[name,status]);
-	this.studentLevel = studentLevel;	
+function Student(name, status, studentLevel) {
+	User.apply(this, [name, status]);
+	this.studentLevel = studentLevel;
 }
 
-Student.prototype =Object.create(User.prototype);
+Student.prototype = Object.create(User.prototype);
 Student.prototype.constructor = Student;
 
 
 // Create the borrowBook prototype method
-User.prototype.borrowBook =  function (bookName,author) {
+User.prototype.borrowBook = function (bookName, author) {
 	requestList.push({
-		'BookName' : bookName,
+		'BookName': bookName,
 		'BookLender': this.name,
-		'Author':author,
-		
+		'Author': author,
+
 	});
-	
+
 }
 
 //Override the returnBook prototype method from the User constructor function.
-User.prototype.returnBook = function(bookName,author) {
+User.prototype.returnBook = function (bookName, author) {
 	return returnList.push({
-		'BookName' : bookName,
+		'BookName': bookName,
 		'BookLender': this.name,
-		'Author':author,
-		});
-	}
+		'Author': author,
+	});
+}
 
 
 module.exports = Student;
