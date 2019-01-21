6eba11c4cfd97aa6e151bf979a42330149a8d1b2
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-21 13:33:14 +0100

    Modify the addbook by sorting the books in alphabetical order, replacebook and borrowbook method

--- person/admin/admin.js
+++ person/admin/admin.js
@@ -1,4 +1,4 @@
-//Import the Person module 
+//Import modules.
 const Person = require('../person');
 const library = require('../../data/library');
 const requestList = require('../../data/requestList');
@@ -13,38 +13,49 @@ function Admin(name) {
 	Person.call(this, name);
 }
 
-
+//Change the prototype name of the admin constructor
 Admin.prototype = Object.create(Person.prototype);
 Admin.prototype.constructor = Admin;
 
 //Create the addBook Prototype method.
 Admin.prototype.addBook = function (bookName, author, quantity) {
+	//Add books to the library
 	library.push({
-		// [`${bookName} by ${author}`] : quantity
 		'BookName': bookName,
 		'Author': author,
 		'Quantity': quantity
 
-	})
+	});
+
+	// Sort the books in the library in aplhabetical order.
+	library.sort(function (a, b) {
+		return a['BookName'].localeCompare(b['BookName']);
+	});
+	return library;
+
 }
 
 
 
 //Prototype method used to issueBook
 Admin.prototype.issueBook = function () {
+	//Sort the request list based on their priority
 	requestList.sort(function (a, b) {
 		return String(a['Priority']).localeCompare(String(b['Priority']));
 	});
 
+	//Check if the book requested is in the library and the issue it if it exist.
 	for (let i = 0; i < requestList.length; i++) {
 		if (binarySearch(library, requestList[i].BookName)) {
 			console.log(`${requestList[i].BookName} issued to  ${requestList[i].BookLender}`);
-			issueList.push(binarySearch(library, requestList[i].BookName));
+			issueList.push(requestList[i]);
+
 		} else {
 			console.log(`${requestList[i].BookLender} : ${requestList[i].BookName} not Found or Taken `)
 		}
 
 	}
+
 
 }
 
@@ -53,10 +64,14 @@ Admin.prototype.replaceBook = function () {
 	for (let i = 0; i < returnList.length; i++) {
 		if (replaceSearch(library, returnList[i].BookName)) {
 			console.log(returnList[i].BookName);
-			replaceList.push(replaceSearch(library, returnList[i].BookName));
+			replaceList.push({
+				'BookName': `${returnList[i].BookName}`,
+				'Author': `${returnList[i].Author}`
+			});
 
-		} 
+		}
 	}
+	
 
 }
 
