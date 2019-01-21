3e7969ee50235aa8de19e9bc2a0e1ae5ed4b4246
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-21 17:42:14 +0100

    Refactored the code to reflect that the inherited methods from the user

--- person/user/teacher/teacher.js
+++ person/user/teacher/teacher.js
@@ -1,9 +1,6 @@
 const User = require('../user');
-const requestList = require('../../../data/requestList');
-const returnList = require('../../../data/returnList');
-const library = require('../../../data/library');
 
-//Create constructor function for the Teacher
+
 function Teacher(name,status,priority) {
 	User.apply(this,[name,status]);	
 	this.priority = priority;
@@ -13,24 +10,5 @@ function Teacher(name,status,priority) {
 Teacher.prototype = Object.create(User.prototype);
 Teacher.prototype.constructor = Teacher;
 
-//Override the borrow prototype method from the User constructor function.
-User.prototype.borrowBook =  function (bookName,author) {
-		requestList.push({
-			'BookName' : bookName,
-			'BookLender': this.name,
-			'Author':author,
-			'Priority' :  this.priority
-		});
-}
-
-//Override the user returnList prototype method
-User.prototype.returnBook = function(bookName,author) {
-	
-	return returnList.push({
-		'BookName' : bookName,
-		'BookLender': this.name,
-		'Author':author,
-	});
-}
 
 module.exports = Teacher;
