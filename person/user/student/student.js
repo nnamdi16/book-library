f04e12915052b1571807a73a3170cc14a55ceed7
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-21 17:39:39 +0100

    Refactored the code to reflect that the junior_student inherited methods from the user

--- person/user/student/student.js
+++ person/user/student/student.js
@@ -1,7 +1,5 @@
 //Import files 
 const User = require('../user');
-const requestList = require('../../../data/requestList');
-const returnList = require('../../../data/returnList');
 
 //Create the Student constructor function
 function Student(name, status, studentLevel) {
@@ -14,25 +12,7 @@ Student.prototype = Object.create(User.prototype);
 Student.prototype.constructor = Student;
 
 
-// Create the borrowBook prototype method
-User.prototype.borrowBook = function (bookName, author) {
-	requestList.push({
-		'BookName': bookName,
-		'BookLender': this.name,
-		'Author': author,
 
-	});
-
-}
-
-//Override the returnBook prototype method from the User constructor function.
-User.prototype.returnBook = function (bookName, author) {
-	return returnList.push({
-		'BookName': bookName,
-		'BookLender': this.name,
-		'Author': author,
-	});
-}
 
 
 module.exports = Student;
