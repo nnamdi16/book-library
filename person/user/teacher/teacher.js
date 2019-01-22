5b6e192d3ad61381f144b1cf1c21927da6722920
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-22 08:58:41 +0100

    Add more comments for code clarification

--- person/user/teacher/teacher.js
+++ person/user/teacher/teacher.js
@@ -1,12 +1,14 @@
+//Import required files
 const User = require('../user');
 
-
+//Creating a teacher constructor
 function Teacher(name,status) {
 	User.apply(this,[name,status]);	
 	this.priority = 1;
 	
 }
 
+//Changing the prototype of the Teacher constructor to Teacher
 Teacher.prototype = Object.create(User.prototype);
 Teacher.prototype.constructor = Teacher;
 
