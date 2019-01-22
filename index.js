3b30e93d54c884f00f761236b205ee1e1cf22761
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-01-22 02:31:29 +0100

    Comment where necessary for clarity in implementation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Import files
 const User = require('./person/user/user');
 const requestList = require('./data/requestList');
 const issueList = require('./data/issueList')
@@ -10,15 +11,21 @@ const JuniorStudent = require('./person/user/student/junior_student/juniorStuden
 const returnList = require('./data/returnList');
 const replaceList = require('./data/replaceList');
 
+//Create various users that want to borrow book
 const user1 = new JuniorStudent('Nnamdi','Student','junior');
 const user4 = new Teacher ('Shakira','Teacher');
 const user2 = new SeniorStudent('Sammy','Student','senior');
 const user3 = new Teacher ('Sophie','Teacher');
 
+//Create admin
 const admin = new Admin('Galvin Belson');
+
+
+//Admin adds book to the library
 admin.addBook('Shades of Home', 'Bachir Lawal', 9);
 admin.addBook('Simple Crazy', 'Antolva Cripal',8);
 
+//User borrows book
 user1.borrowBook('Shades of Home', 'Bachir Lawal');
 user4.borrowBook('Simple Crazy', 'Antolva Cripal');
 user2.borrowBook('Fall An Emperor', 'Goodwill Sand');
@@ -26,25 +33,26 @@ user3.borrowBook('Simple Crazy', 'Antolva Cripal');
 
 
 
-
+//Check the list of books in the library
 console.log('List of books in the library',library);
 
 
-// // console.log(user1);
+//Shows the list of user who wants to borrow a book and the author
+console.log('Request List',requestList);
 
-
-
-// console.log('Request List',requestList);
+//Issue Book Implementation
 admin.issueBook();
-console.log('Request List after book issued ',requestList);
+// console.log('Request List after book issued ',requestList);
 console.log('Library', library);
-// // console.log('Lib',library);
+// // // console.log('Lib',library);
 console.log('List of books issued',issueList);
+
+//Return book Implementation
 user1.returnBook('Simple Crazy', 'Antolva Cripal');
 user3.returnBook('Simple Crazy', 'Antolva Cripal');
+console.log('Library return', returnList);
 
-// console.log('Library return', returnList);
+//Replace book Implementation.
 admin.replaceBook();
-
-console.log('library',library);
+console.log('List of books in the library after return of borrowed books',library);
 console.log('ReplaceList',replaceList);
